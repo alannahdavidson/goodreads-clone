65cf59687f6d38eb58d82b8002a5b6352bef2006
Alannah Davidson <alannahdavidson7@protonmail.com>
2024-01-07 13:30:55 +1100

    GoodReads project initialisation

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Alan is the best
         </a>
       </header>
     </div>
